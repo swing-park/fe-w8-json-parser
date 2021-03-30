a5c7cd1c8980ec030ee49c34e2fea443185f4763
swing-park <ongsim123@gmail.com>
2021-03-30 18:44:54 +0900

    Fix : 파일 이름 수정 및 경로 수정

---
+++ src/components/json-parser/lexer.js
@@ -0,0 +1,29 @@
+import { regex } from "../../utils/regex.js";
+
+const lexicalize = (tokens) => {
+	return tokens.reduce((acc, token) => {
+		if (token === "[" || token === "]") {
+			acc.push({ type: "array", child: [], value: "arrayObject", id: token });
+		} else if (token === "{" || token === "}") {
+			acc.push({ type: "object", child: [], id: token });
+		} else if (token === "null") {
+			acc.push({ type: "object", value: null });
+		} else if (token === "undefined") {
+			acc.push({ type: "undefined", value: undefined });
+		} else if (token === "true") {
+			acc.push({ type: "boolean", value: true });
+		} else if (token === "false") {
+			acc.push({ type: "boolean", value: false });
+		} else if (token === ":") {
+			acc.push({ type: "colon", value: "colon" });
+		} else if (token.includes('"') || token.includes("'")) {
+			acc.push({ type: "string", value: token.replace(/\"/g, "") });
+		} else {
+			// number
+			acc.push({ type: "number", value: Number(token) });
+		}
+		return acc;
+	}, []);
+};
+
+export { lexicalize };
---
+++ src/public/index.js
@@ -0,0 +1,5 @@
+import App from "../app.js";
+import "./index.scss";
+
+const $app = document.querySelector("#app");
+new App({ $target: $app });
