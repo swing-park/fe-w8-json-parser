75ced794ed88832653367dd0bdfad896c9e163af
adelakim <diddnjs02@likelion.org>
2021-03-31 18:52:20 +0900

    [Chore] Remove unnecessary comments
    - 불필요한 주석 제거
    - parser에서 사용하는 cond를 utils에 옮김, cond -> condition이라고 수정
    - go() 생성
        - utils에 배치
        - 함수 합성
        - 현재 로직: userInput을 받아 tokenizer, lexer, parser를 거쳐 JSON.stringify 해준 결과값(트리 형태의 문자열)을 반환
        - main.js에서 테스트해봄, 문제없는 것으로 확인

--- src/js/parser.js
+++ src/js/parser.js
@@ -1,25 +1,14 @@
-const cond = {
-  ARRAY: ({ type }) => type === "array",
-  OPENARR: ({ id }) => id === "[",
-  CLOSEARR: ({ id }) => id === "]",
-  OBJECT: ({ type }) => type === "object",
-  OBJECTPROP: ({ type }) => type === "objectProperty",
-  OPENOBJ: ({ id }) => id === "{",
-  CLOSEOBJ: ({ id }) => id === "}",
-  STRING: ({ type }) => type === "string",
-  COLON: ({ type }) => type === "colon",
-  OTHER: ({ type }) => type === "number" || type === "undefined" || type === "boolean",
-};
+import { condition } from "./utils.js";
 
 const parse = (lexes, parentNode = null, idx = 0) => {
   for (let i = idx; i < lexes.length; ) {
     const lex = lexes[i];
 
-    if (cond.COLON(lex)) i++;
+    if (condition.COLON(lex)) i++;
 
-    if (cond.ARRAY(lex)) {
+    if (condition.ARRAY(lex)) {
       const currNode = { type: lex.type, child: lex.child, value: lex.value };
-      if (cond.OPENARR(lex)) {
+      if (condition.OPENARR(lex)) {
         const newIdx = parse(lexes, currNode, i + 1);
         if (!parentNode) return currNode;
         if (parentNode.type === "objectProperty") {
@@ -30,10 +19,10 @@ const parse = (lexes, parentNode = null, idx = 0) => {
         i = newIdx;
       }
 
-      if (cond.CLOSEARR(lex)) return i + 1;
+      if (condition.CLOSEARR(lex)) return i + 1;
     }
 
-    if (cond.OBJECT(lex)) {
+    if (condition.OBJECT(lex)) {
       if (!lex.id) {
         const currNode = { type: lex.type, value: lex.value };
         parentNode.child.push(currNode);
@@ -42,7 +31,7 @@ const parse = (lexes, parentNode = null, idx = 0) => {
       }
 
       const currNode = { type: lex.type, child: lex.child };
-      if (cond.OPENOBJ(lex)) {
+      if (condition.OPENOBJ(lex)) {
         const newIdx = parse(lexes, currNode, i + 1);
         if (!parentNode) return currNode;
         if (parentNode.type === "objectProperty") {
@@ -53,12 +42,12 @@ const parse = (lexes, parentNode = null, idx = 0) => {
         i = newIdx;
       }
 
-      if (cond.CLOSEOBJ(lex)) return i + 1;
+      if (condition.CLOSEOBJ(lex)) return i + 1;
     }
 
-    if (cond.STRING(lex)) {
+    if (condition.STRING(lex)) {
       const currNode = { type: lex.type, value: lex.value };
-      if (cond.COLON(lexes[i + 1])) {
+      if (condition.COLON(lexes[i + 1])) {
         const currObj = {
           value: {
             propKey: currNode,
@@ -70,7 +59,7 @@ const parse = (lexes, parentNode = null, idx = 0) => {
         const newIdx = parse(lexes, currObj, i + 1);
         parentNode.child.push(currObj);
         i = newIdx;
-      } else if (cond.COLON(lexes[i - 1])) {
+      } else if (condition.COLON(lexes[i - 1])) {
         parentNode.value.propValue = currNode;
         return i + 1;
       } else {
@@ -83,7 +72,7 @@ const parse = (lexes, parentNode = null, idx = 0) => {
       }
     }
 
-    if (cond.OTHER(lex)) {
+    if (condition.OTHER(lex)) {
       const currNode = { type: lex.type, value: lex.value };
       if (!parentNode) return currNode;
       if (parentNode.type === "objectProperty") {
@@ -96,89 +85,5 @@ const parse = (lexes, parentNode = null, idx = 0) => {
   }
   return parentNode;
 };
-
-// 이전 코드
-// const parse = (lexes) => {
-//   const stack = [];
-
-//   for (let i = 0; i < lexes.length; i++) {
-//     const lex = lexes[i];
-//     if (lex.type === "array") {
-//       if (lex.id === "[") {
-//         stack.push({ type: lex.type, child: lex.child, value: lex.value });
-//       } else if (lex.id === "]") {
-//         const child = stack.pop();
-//         if (stack.length) {
-//           if (stack[stack.length - 1].type === "objectProperty") {
-//             stack[stack.length - 1].value.propValue = child;
-//             stack[stack.length - 2].child.push(stack[stack.length - 1]);
-//             stack.pop();
-//           } else if (stack[stack.length - 1].type === "array" || stack[stack.length - 1].type === "object") stack[stack.length - 1].child.push(child);
-//         } else return child;
-//       }
-//       continue;
-//     }
-
-//     if (lex.type === "object") {
-//       if (!lex.id) {
-//         stack[stack.length - 1].child.push({ type: lex.type, value: lex.value });
-//         continue;
-//       }
-//       if (lex.id === "{") {
-//         stack.push({ type: lex.type, child: lex.child });
-//       } else {
-//         const child = stack.pop();
-//         if (stack.length) {
-//           if (stack[stack.length - 1].type === "objectProperty") {
-//             stack[stack.length - 1].value.propValue = child;
-//             stack[stack.length - 2].child.push(stack[stack.length - 1]);
-//             stack.pop();
-//           } else if (stack[stack.length - 1].type === "array" || stack[stack.length - 1].type === "object") stack[stack.length - 1].child.push(child);
-//         } else return child;
-//       }
-//       continue;
-//     }
-
-//     if (lex.type === "string") {
-//       if (lexes[i + 1].type === "colon") {
-//         // key??
-//         const obj = {
-//           value: null,
-//           type: "objectProperty",
-//         };
-//         obj.value = {
-//           propKey: { type: lex.type, value: lex.value },
-//           propValue: null,
-//         };
-//         stack.push(obj);
-//       } else if (lexes[i - 1].type === "colon") {
-//         // value??
-//         stack[stack.length - 1].value.propValue = { type: lex.type, value: lex.value };
-//         const child = stack.pop();
-//         stack[stack.length - 1].child.push(child);
-//       } else {
-//         const element = { type: lex.type, value: lex.value };
-//         if (stack[stack.length - 1].type === "objectProperty") {
-//           stack[stack.length - 1].value.propValue = element;
-//           stack[stack.length - 2].child.push(stack[stack.length - 1]);
-//           stack.pop();
-//         } else if (stack[stack.length - 1].type === "array" || stack[stack.length - 1].type === "object") stack[stack.length - 1].child.push(element);
-//         else stack.push(element);
-//       }
-//       continue;
-//     }
-
-//     if (lex.type === "number" || lex.type === "undefined" || lex.type === "boolean") {
-//       const element = { type: lex.type, value: lex.value };
-//       if (stack[stack.length - 1].type === "objectProperty") {
-//         stack[stack.length - 1].value.propValue = element;
-//         stack[stack.length - 2].child.push(stack[stack.length - 1]);
-//         stack.pop();
-//       } else if (stack[stack.length - 1].type === "array" || stack[stack.length - 1].type === "object") stack[stack.length - 1].child.push(element);
-//       else stack.push(element);
-//     }
-//   }
-//   // console.log(stack);
-// };
 
 export { parse };
