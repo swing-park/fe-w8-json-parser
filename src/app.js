16476820d9670c925afb2d3879c0e03e168d0983
swing <69034766+swing-park@users.noreply.github.com>
2021-03-31 14:50:27 +0900

    [swindela] tokenizer & lexer 구현 완료  . parser 구현 중 (#20)
    * [Add] Set environment - webpack
    
    - webpack
    - babel
    - express
    - webpack-dev-server
    - webpack-dev-middleware
    
    * [Add] readme.md
    
    - 설치 방법 작성
    
    * Update readme.md
    
    * Update readme.md
    
    * [Fix] webpack
    
    - dist에 이전 번들 파일이 남지 않도록 수정
        - devServer의 writeToDisk 삭제
    
    * Update readme.md
    
    * Update readme.md
    
    * Fix : readme
    
    * Add : tokenizer
    - 배열로 들어온 문자열을 분석화해서 Token화 해주는 함수 구현
    - object 분석은 구상 중
    
    * [Create] lexer, parser
    
    1. tokenizer
    - 의미 부여 없이 토큰화한 값을 담은 배열 반환
    2. lexer
    - token별로 type, value값 부여하여 배열 반환
    3. parser
    - lexer에서 반환한 배열을 기반으로 트리 구조로 구성하는 중
    - 자식이 있는 경우 재귀로 자식구조를 만든 후 리턴하여 부모의 child에 넣는 방식으로 구상 중
    - 난항 중
    
    * Fix : entry file 경로 수정
    
    * Fix : 파일 이름 수정 및 경로 수정
    
    * Add : view & model 구현을 위한 container , presentational 구조 잡고 구현 중
    
    * Fix : entry file이 될 js 지정
    
    Co-authored-by: adelakim <diddnjs02@likelion.org>
    Co-authored-by: adelakim5 <49264795+adelakim5@users.noreply.github.com>

---
+++ src/app.js
@@ -0,0 +1,17 @@
+import { inputContainer } from "./components/input/inputContainer.js"
+import { analysisContainer } from "./components/analysis/analysisContainer.js"
+
+export default class App {
+	constructor($target) {
+		this.components = null;
+
+	}
+
+	setState() {
+
+	}
+
+	render() {
+
+	}
+}
---
+++ src/components/input/inputContainer.js
@@ -0,0 +1,17 @@
+import { inputInfo } from "./infoPresentational.js"
+import { inputLog } from "./userInputPresentational.js.js"
+import { analysisButton } from "./analysisButtonPresentational.js"
+
+export const inputContainer = ({ $target }) => {
+	const $input_section = document.createElement("section")
+	$input_section.className = "input-section"
+	$target.appendChild($inputSection);
+
+	render({ $target: $input_section })
+};
+
+const render = ({ $target }) => {
+	inputInfo($target);
+	inputLog($target);
+	analysisButton($target);
+};
