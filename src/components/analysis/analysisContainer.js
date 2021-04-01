4708d051be6576bbd6f054d0192557a431cb56c4
swing-park <ongsim123@gmail.com>
2021-04-01 13:26:43 +0900

    Fix : 레거시 삭제

--- src/components/analysis/analysisContainer.js
+++
@@ -1,1 +0,0 @@
-export const analysisContainer = ({ $target, data }) => { }
--- src/utils/regex.js
+++ src/utils/regex.js
@@ -2,8 +2,3 @@ export const regex = {
 	enter: /(\r\n\t|\n|\r\t)/g,
 	sementic: /('.*?'|".*?"|\s+|\[|\{|\}|\]|\d+|null|true|false|undefined|\:|\,)/g,
 };
-
-export const userInput = `["la3", [null, false, ["11", [112233], { "easy": ["hello", { "a": "a" }, "world"] }, 112], 55, "99"],
-{ "a": "str", "b": [912, [5656, 33], { "key": "inner value", "newkeys": [1, 2, 3, 4, 5] }] }, true]`;
-
-({ type: "array", child: [], value: "arrayObject" })
