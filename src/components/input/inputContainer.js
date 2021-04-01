4ee12b41d358842a5cebb991beb8b14fb9ef400d
adelakim <diddnjs02@likelion.org>
2021-04-01 11:52:44 +0900

    [Refactor] Relocate and edit functions
    - inputContainer에 go로 합성한 함수를 사용
    - 추후 json-parser에 있는 main.js를 삭제해도 좋을 것
    - 불필요한 소스파일을 제거
    - 불필요한 주석 제거

--- src/components/input/inputContainer.js
+++ src/components/input/inputContainer.js
@@ -1,72 +1,74 @@
-import OutputContainer from "../output/outputContainer.js"
-import InputPresentational from "./inputPresentational.js"
+import OutputContainer from "../output/outputContainer.js";
+import InputPresentational from "./inputPresentational.js";
 import { tokenize } from "../json-parser/tokenizer.js";
 import { lexicalize } from "../json-parser/lexer.js";
 import { parse } from "../json-parser/parser.js";
-import "./input.scss"
+// import {parseJson} from "../json-parser/main.js"
+import "./input.scss";
+import { pipe } from "../../utils/utils.js";
 
 class InputContainer {
-	constructor({ $target }) {
-		this.presentationals = null;
-		this.buttonStatus = false;
+  constructor({ $target }) {
+    this.presentationals = null;
+    this.buttonStatus = false;
 
-		//parser함수 이니셜라이즈
-		this.tokenize = tokenize;
-		this.lexicalize = lexicalize;
-		this.parse = parse;
+    //parser함수 이니셜라이즈
+    this.tokenize = tokenize;
+    this.lexicalize = lexicalize;
+    this.parse = parse;
 
-		//output notify를 위한 Container init
-		// this.output = OutputContainer;
-		// console.log(this.output.test)
+    //output notify를 위한 Container init
+    // this.output = OutputContainer;
+    // console.log(this.output.test)
 
-		this.$input = document.createElement("section");
-		this.$input.className = "input-section";
-		$target.appendChild(this.$input)
+    this.$input = document.createElement("section");
+    this.$input.className = "input-section";
+    $target.appendChild(this.$input);
 
-		this.setState();
-	}
+    this.setState();
+  }
 
-	setState() {
-		this.render()
-	}
+  setState() {
+    this.render();
+  }
 
-	handleUserInput() {
-		// if (this.buttonStatus) {
-		// 	console.log("helo'")
-		// 	this.buttonStatus = false;
-		// } else {
-		// 	this.buttonStatus = true;
-		// }
+  handleUserInput() {
+    // if (this.buttonStatus) {
+    // 	console.log("helo'")
+    // 	this.buttonStatus = false;
+    // } else {
+    // 	this.buttonStatus = true;
+    // }
+    // this.setState();
+  }
 
-		// this.setState();
-	}
+  handleAnalysisButton() {
+    const inputValue = this.getInputValue();
+    const result = go(inputValue, tokenize, lexicalize, parse);
+    // const tokens = this.tokenize(inputValue);
+    // const lexicalizedTokens = this.lexicalize(tokens);
+    // const parsedData = this.parse(lexicalizedTokens);
+    // console.log(parsedData)
+    // const a = JSON.stringify(parsedData, null, ' ')
+    // console.log(a)
+    document.querySelector(".analysis-log").innerHTML = "";
+    document.querySelector(".analysis-log").innerText = JSON.stringify(result, null, " ");
+  }
 
-	handleAnalysisButton() {
-		const inputValue = this.getInputValue();
-		const tokens = this.tokenize(inputValue);
-		const lexicalizedTokens = this.lexicalize(tokens);
-		const parsedData = this.parse(lexicalizedTokens);
-		console.log(parsedData)
-		const a = JSON.stringify(parsedData, null, ' ')
-		console.log(a)
-		document.querySelector(".analysis-log").innerHTML = "";
-		document.querySelector(".analysis-log").innerText = a;
-	}
+  getInputValue() {
+    return this.$input.querySelector(".user-input").value;
+  }
 
-	getInputValue() {
-		return this.$input.querySelector(".user-input").value
-	}
-
-	render() {
-		this.presentationals = {
-			input: new InputPresentational({
-				$target: this.$input,
-				buttonStatus: this.buttonStatus,
-				onKeyupUserInput: this.handleUserInput.bind(this),
-				onClickAnalysisButton: this.handleAnalysisButton.bind(this)
-			})
-		};
-	}
+  render() {
+    this.presentationals = {
+      input: new InputPresentational({
+        $target: this.$input,
+        buttonStatus: this.buttonStatus,
+        onKeyupUserInput: this.handleUserInput.bind(this),
+        onClickAnalysisButton: this.handleAnalysisButton.bind(this),
+      }),
+    };
+  }
 }
 
-export default InputContainer
+export default InputContainer;
--- src/components/output/analysisLog/analysisLogPresentational.js
+++ src/components/output/analysisLog/analysisLogPresentational.js
@@ -1,17 +1,17 @@
 class AnalysisLogPresentational {
-	constructor({ $target }) {
-		this.render($target)
-	}
+  constructor({ $target }) {
+    this.render($target);
+  }
 
-	render($target) {
-		const $analysisLog_section = document.createElement("section");
-		$analysisLog_section.className = "analysis-log-section";
-		$target.appendChild($analysisLog_section);
+  render($target) {
+    const $analysisLog_section = document.createElement("section");
+    $analysisLog_section.className = "analysis-log-section";
+    $target.appendChild($analysisLog_section);
 
-		const $analysisLog = `<div class="analysis-log">분석 결과</div>`
+    const $analysisLog = `<pre class="analysis-log">분석 결과</pre>`;
 
-		$analysisLog_section.insertAdjacentHTML("beforeend", $analysisLog);
-	}
+    $analysisLog_section.insertAdjacentHTML("beforeend", $analysisLog);
+  }
 }
 
-export default AnalysisLogPresentational
+export default AnalysisLogPresentational;
