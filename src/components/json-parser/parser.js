013b8a7a60a3ae237313719a19cf57bd4b3e9160
adelakim <diddnjs02@likelion.org>
2021-04-01 20:01:54 +0900

    [Refactor] parser
    - parse함수 리팩토링
        - pushChild라는 내부 함수 생성
        - 재귀를 돌리고 반환받는 로직을 pushChild 함수에서 진행
        - 비슷한 로직을 수행하는 조건끼리 묶음
        - 여전히 중복되는 로직이 존재하지만 현재로서는 이게 최선인듯함..

--- src/components/json-parser/parser.js
+++ src/components/json-parser/parser.js
@@ -1,89 +1,48 @@
 import { condition } from "../../utils/utils.js";
 
 const parse = (lexes, parentNode = null, idx = 0) => {
-	for (let i = idx; i < lexes.length;) {
-		const lex = lexes[i];
+  const pushChild = (currNode, i, fn = undefined) => {
+    const newIdx = !fn ? i + 1 : fn(lexes, currNode, i + 1);
+    if (!parentNode) return currNode;
+    if (parentNode.type === "objectProperty") parentNode.value.propValue = currNode;
+    if (parentNode.type === "array" || parentNode.type === "object") parentNode.child.push(currNode);
+    return newIdx;
+  };
 
-		if (condition.COLON(lex)) i++;
+  for (let i = idx; i < lexes.length; ) {
+    const lex = lexes[i];
 
-		if (condition.ARRAY(lex)) {
-			const currNode = { type: lex.type, child: lex.child, value: lex.value };
-			if (condition.OPENARR(lex)) {
-				const newIdx = parse(lexes, currNode, i + 1);
-				if (!parentNode) return currNode;
-				if (parentNode.type === "objectProperty") {
-					parentNode.value.propValue = currNode;
-					return newIdx;
-				}
-				if (parentNode.type === "array" || parentNode.type === "object") parentNode.child.push(currNode);
-				i = newIdx;
-			}
+    if (condition.COLON(lex)) i++;
 
-			if (condition.CLOSEARR(lex)) return i + 1;
-		}
+    if ((condition.ARRAY(lex) && condition.OPENARR(lex)) || (condition.OBJECT(lex) && condition.OPENOBJ(lex))) {
+      const currNode = condition.ARRAY(lex) ? { type: lex.type, child: lex.child, value: lex.value } : { type: lex.type, child: lex.child };
+      const result = pushChild(currNode, i, parse);
+      if (parentNode && parentNode.type !== "objectProperty") i = result;
+      else return result;
+    }
 
-		if (condition.OBJECT(lex)) {
-			if (!lex.id) {
-				const currNode = { type: lex.type, value: lex.value };
-				parentNode.child.push(currNode);
-				i++;
-				continue;
-			}
+    if ((condition.ARRAY(lex) && condition.CLOSEARR(lex)) || (condition.OBJECT(lex) && condition.CLOSEOBJ(lex))) return i + 1;
 
-			const currNode = { type: lex.type, child: lex.child };
-			if (condition.OPENOBJ(lex)) {
-				const newIdx = parse(lexes, currNode, i + 1);
-				if (!parentNode) return currNode;
-				if (parentNode.type === "objectProperty") {
-					parentNode.value.propValue = currNode;
-					return newIdx;
-				}
-				if (parentNode.type === "array" || parentNode.type === "object") parentNode.child.push(currNode);
-				i = newIdx;
-			}
+    if (condition.OBJECT(lex) && !lex.id) {
+      parentNode.child.push({ type: lex.type, value: lex.value });
+      i++;
+    }
 
-			if (condition.CLOSEOBJ(lex)) return i + 1;
-		}
-
-		if (condition.STRING(lex)) {
-			const currNode = { type: lex.type, value: lex.value };
-			if (condition.COLON(lexes[i + 1])) {
-				const currObj = {
-					value: {
-						propKey: currNode,
-						propValue: null,
-					},
-					type: "objectProperty",
-				};
-
-				const newIdx = parse(lexes, currObj, i + 1);
-				parentNode.child.push(currObj);
-				i = newIdx;
-			} else if (condition.COLON(lexes[i - 1])) {
-				parentNode.value.propValue = currNode;
-				return i + 1;
-			} else {
-				if (parentNode.type === "objectProperty") {
-					parentNode.value.propValue = currNode;
-					return i + 1;
-				}
-				if (parentNode.type === "array" || parentNode.type === "object") parentNode.child.push(currNode);
-				i++;
-			}
-		}
-
-		if (condition.OTHER(lex)) {
-			const currNode = { type: lex.type, value: lex.value };
-			if (!parentNode) return currNode;
-			if (parentNode.type === "objectProperty") {
-				parentNode.value.propValue = currNode;
-				return i + 1;
-			}
-			if (parentNode.type === "array" || parentNode.type === "object") parentNode.child.push(currNode);
-			i++;
-		}
-	}
-	return parentNode;
+    if (condition.STRING(lex) || condition.OTHER(lex)) {
+      const currNode = { type: lex.type, value: lex.value };
+      if (condition.COLON(lexes[i + 1])) {
+        i = pushChild({ value: { propKey: currNode, propValue: null }, type: "objectProperty" }, i, parse);
+      } else if (condition.COLON(lexes[i - 1])) {
+        parentNode.value.propValue = currNode;
+        return i + 1;
+      } else {
+        const result = pushChild(currNode, i);
+        if (parentNode && parentNode.type !== "objectProperty") i = result;
+        else return result;
+      }
+    }
+  }
+  return parentNode;
 };
 
 export { parse };
