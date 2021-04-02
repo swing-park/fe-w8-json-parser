ecde211481671d912cffcbbd0f7a23da8857dc88
adelakim <diddnjs02@likelion.org>
2021-04-02 12:20:22 +0900

    [Chore] 불필요한 파일 삭제 및 간단한 퍼블리싱 작업

--- src/components/input/inputInfo/inputInfoPresentational.js
+++ src/components/input/inputInfo/inputInfoPresentational.js
@@ -1,17 +1,17 @@
 class InputInfoPresentational {
-	constructor({ $target }) {
-		this.render($target)
-	}
+  constructor({ $target }) {
+    this.render($target);
+  }
 
-	render($target) {
-		const $inputInfo_section = document.createElement("section");
-		$inputInfo_section.className = "input-info-section";
-		$target.appendChild($inputInfo_section);
+  render($target) {
+    const $inputInfo_section = document.createElement("section");
+    $inputInfo_section.className = "input-info-section";
+    $target.appendChild($inputInfo_section);
 
-		const $inputInfo = `<span class="input-info">JSON 데이터를 추가해주세요</span>`
+    const $inputInfo = `<span class="input-info">JSON 데이터를 추가해 주십시오!</span>`;
 
-		$inputInfo_section.insertAdjacentHTML("beforeend", $inputInfo);
-	}
-};
+    $inputInfo_section.insertAdjacentHTML("beforeend", $inputInfo);
+  }
+}
 
-export default InputInfoPresentational
+export default InputInfoPresentational;
--- src/components/output/analysisInfo/analysisInfoPresentational.js
+++ src/components/output/analysisInfo/analysisInfoPresentational.js
@@ -1,17 +1,17 @@
 class AnalysisInfoPresentational {
-	constructor({ $target }) {
-		this.render($target)
-	}
+  constructor({ $target }) {
+    this.render($target);
+  }
 
-	render($target) {
-		const $analysisInfo_section = document.createElement("section");
-		$analysisInfo_section.className = "analysis-info-section";
-		$target.appendChild($analysisInfo_section);
+  render($target) {
+    const $analysisInfo_section = document.createElement("section");
+    $analysisInfo_section.className = "analysis-info-section";
+    $target.appendChild($analysisInfo_section);
 
-		const $analysisInfo = `<span class="analysis-info">분석 결과입니다</span>`
+    const $analysisInfo = `<span class="analysis-info">분석 결과를 확인하십시오.</span>`;
 
-		$analysisInfo_section.insertAdjacentHTML("beforeend", $analysisInfo);
-	}
+    $analysisInfo_section.insertAdjacentHTML("beforeend", $analysisInfo);
+  }
 }
 
-export default AnalysisInfoPresentational
+export default AnalysisInfoPresentational;
